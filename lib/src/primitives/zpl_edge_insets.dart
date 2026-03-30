3391ad88b7f2eee69cacbd307354406044c08a35
Farook Jamal K <farookyj10@gmail.com>
2026-03-30 18:18:13 +0530

    chore: add LICENSE and finalize package metadata for v0.0.1

--- lib/src/primitives/zpl_edge_insets.dart
+++ lib/src/primitives/zpl_edge_insets.dart
@@ -5,16 +5,16 @@ class ZplEdgeInsets {
   final double bottom;
 
   const ZplEdgeInsets.all(double value)
-    : left = value,
-      top = value,
-      right = value,
-      bottom = value;
+      : left = value,
+        top = value,
+        right = value,
+        bottom = value;
 
   const ZplEdgeInsets.symmetric({double vertical = 0, double horizontal = 0})
-    : left = horizontal,
-      top = vertical,
-      right = horizontal,
-      bottom = vertical;
+      : left = horizontal,
+        top = vertical,
+        right = horizontal,
+        bottom = vertical;
 
   const ZplEdgeInsets.only({
     this.left = 0,
